6c3ba23554694dd41b9993673cc7a408e4c03659
alisa260785@gmail.com <alisa260785@gmail.com>
2020-08-05 13:38:53 +0800

    users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,43 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:edit, :destroy, :update, :index]
+
   def index
+   @post = Post.all
+  end
+
+def new
+    @user = User.new
+      
+    end
+
+def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to user_path
+    else
+      render :new
+    end
+end
+
+
+def edit
+end
+
+def update
+  # if @user.update(user_params)
+  #   redirect_to user_path
+  # else
+  #   render :edit
+  # end
+
+end
+private
+
+  def set_user
+    @user = current_user
+  end
+
+  def user_params
+    params.require(:user).permit(:fname, :lname, :desc, :username, :password,:email, :phone, :address, :city, :country)
   end
 end
