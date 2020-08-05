2807ea35b77b2311dddbc3f4b4bf11b4f3589f73
metildachee <metildachee@gmail.com>
2020-08-05 22:12:27 +0800

    unfollow and follow people ok

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,19 +4,32 @@ class UsersController < ApplicationController
   before_action :set_user, only: [:edit, :destroy, :update, :index]
 
   def show
+
+    # initialise
     @followers = Array.new
     @following = Array.new
-    
+    @following_this_person = false
+    @followed_by_this_person = false
+
+
     @user = User.find(params[:id]) # this is the person who is currently being shown
-    followers = Relationship.where(followed_id: @user.id) # we find the Relationships where users follow current user
+    followers = Relationship.where(followed_id: @user.id) # we find the Relationships of users follow current user
     following = Relationship.where(follower_id: @user.id) # we find the Relationships of users who this current user follows
 
     # using the id, we find the Users and push them into an array so that the front-end can render
     followers.each do |follower|
+      if (follower.follower_id == current_user.id) # this particular user contains current user's ID => current user follows this person
+        @following_this_person = true
+      end 
+
       @followers.push(User.find(follower.follower_id)) 
     end 
 
     following.each do |following|
+      if (following.followed_id == current_user.id) 
+        @followed_by_this_person = true;
+      end 
+
       @following << User.find(following.followed_id)
     end 
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+
   # get 'posts/index'
   # get 'posts/create'
   # get 'posts/new'
@@ -11,6 +12,9 @@ Rails.application.routes.draw do
   # get 'users/show'
 
   resources :users do 
+    get 'relationships/create'
+    get 'relationships/destroy'
+
     resources :posts
   end 
   
