e02cd1e912020347b614195e781c7b83e4f799b5
alisa260785@gmail.com <alisa260785@gmail.com>
2020-08-06 17:08:38 +0800

    hastag

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,17 @@
 class Post < ApplicationRecord
+  has_many :post_hash_tags
+  has_many :hash_tags, through: :post_hash_tags
   belongs_to :user
   has_many :comments
   has_one_attached :main_image
+  after_commit :create_hash_tags, on: :create
+  def create_hash_tags
+      extract_name_hash_tags.each do |name|
+      hash_tags.create(name: name)
+      end
+    end
+  def extract_name_hash_tags
+      description.to_s.scan(/#\w+/).map{|name| name.gsub("#", "")}
+    end
 
 end
