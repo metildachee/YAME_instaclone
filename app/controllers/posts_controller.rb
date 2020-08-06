a7c1cf550c4130842e0514405c06188085c48d2f
metildachee <metildachee@gmail.com>
2020-08-06 13:01:10 +0800

    fixed routing for picture posting

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,7 +7,7 @@ class PostsController < ApplicationController
     @post = Post.new(post_params)
     @post["user_id"] = current_user.id
     if @post.save
-      redirect_to posts_index_path
+      redirect_to users_path
     else
       render :new
     end
